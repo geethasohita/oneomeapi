1c77294d1596b5e2f7a7a8a7cd6ca4472fd00e64
geetha sohita <geethasohita@gmail.com>
2021-07-11 15:52:26 -0500

    modified create_vaccine service

--- app/service.py
+++ app/service.py
@@ -4,7 +4,7 @@ from app.models import Vaccine
 
 
 def create_vaccine(name, company, min_age, max_age, fda_approved):
-    existing_vaccine = Vaccine.query.filter(Vaccine.vaccine_name == name)
+    existing_vaccine = get_vaccines(name)
     if existing_vaccine.count() > 0:
         raise BadRequestException(f'Vaccine with name {name} already exists in database.')
     vaccine = Vaccine(vaccine_name=name, produced_company=company, min_age=min_age, max_age=max_age,
@@ -17,8 +17,6 @@ def create_vaccine(name, company, min_age, max_age, fda_approved):
 def get_vaccines(name):
     if name:
         vaccines = Vaccine.query.filter(Vaccine.vaccine_name == name)
-        if vaccines.count() == 0:
-            raise BadRequestException(f'Vaccine with name {name} does not exist in database')
     else:
         vaccines = Vaccine.query.all()
     return vaccines
