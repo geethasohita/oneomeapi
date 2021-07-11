7f733812c48f46853cc2192a867068e26f3d298a
geetha sohita <geethasohita@gmail.com>
2021-07-10 23:20:39 -0500

    adding changes for connecting to heroku postgres

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,17 @@
+import os
+
 from flask import Flask
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5433/oneome'
+
+db_url = os.environ.get('DATABASE_URL', None)
+if not db_url:
+    db_url = 'postgresql://postgres:password@localhost:5433/oneome'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 ma = Marshmallow(app)
